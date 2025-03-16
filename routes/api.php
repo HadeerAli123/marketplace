085752b9f282be7ee51f796d52555c6f51e11337
fatma mohamed <fatmamohamed2101@gmail.com>
2025-03-17 01:19:15 +0200

    test

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test', function (Request $request) {
+Route::get('test', function (Request $request) {
     return response()->json([
         'message' => 'API is working!',
         'status' => true
