3bcf7f7b8f87893d7a5db04b088930034e861810
fatma mohamed <fatmamohamed2101@gmail.com>
2025-05-01 22:59:51 +0300

    deel

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -309,16 +309,13 @@ class ProductController extends Controller
             return response()->json(['error' => 'Failed to update product: ' . $e->getMessage()], 500);
         }
     }
-    public function destroy(Product $id)
+    public function destroy(Product $product)
     {
-        $product = Product::withTrashed()->find($id);
-    
         if (!$product) {
             return response()->json(['message' => 'Product not found.'], 404);
         }
-    
-        $product->forceDelete();
-        return response()->json(['message' => 'Product hard deleted successfully.']);
+        $product->delete();
+        return response()->json(['message' => 'Product soft deleted successfully.']);
     }
 }
 
