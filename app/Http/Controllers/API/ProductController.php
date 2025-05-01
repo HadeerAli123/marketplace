c277d9e8684950e94440cd7424d7372c1af7824f
fatma mohamed <fatmamohamed2101@gmail.com>
2025-05-01 22:56:45 +0300

    force delete

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -311,11 +311,14 @@ class ProductController extends Controller
     }
     public function destroy(Product $product)
     {
+        $product = Product::withTrashed()->find($id);
+    
         if (!$product) {
             return response()->json(['message' => 'Product not found.'], 404);
         }
-        $product->delete();
-        return response()->json(['message' => 'Product soft deleted successfully.']);
+    
+        $product->forceDelete();
+        return response()->json(['message' => 'Product hard deleted successfully.']);
     }
 }
 
